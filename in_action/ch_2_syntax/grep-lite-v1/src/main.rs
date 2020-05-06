1ee4ffcdad03839a6437b9ac5e96b6d3f58e8ad4
Leland Krych <leland.krych@gmail.com>
2020-05-06 09:11:52 -0700

    grep-lite-v2

---
+++ in_action/ch_2_syntax/grep-lite-v1/src/main.rs
@@ -0,0 +1,27 @@
+fn main() {
+    let search_term = "picture";
+    let quote = "Every face, every shop, bedroom window, public house, and
+    dark square is a picture feverishly turned -- in search of what?
+    It is the same with books. What do we seek through millions of pages?";
+
+    for line in quote.lines() {
+        if line.contains(search_term) {
+            println!("{}", line);
+        }
+    }
+}
+
+//another way
+// fn option() {
+//     let search_term = "picture";
+//     let quote = "Every face, every shop, bedroom window, public-house, and
+//     dark square is a picture feverishly turned--in search of what?
+//     It is the same with books. What do we seek through millions of pages?";
+    
+//     for (idx, line) in quote.lines().enumerate() {
+//         if line.contains(search_term) {
+//             let line_num = idx + 1;
+//             println!("{}: {}", line_num, line); 
+//         }
+//     } 
+// }
