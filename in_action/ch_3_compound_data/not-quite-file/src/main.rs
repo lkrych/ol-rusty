e7dc0b394e9e97f290a8df8f0fce6ac76ced2f63
Leland Krych <leland.krych@gmail.com>
2020-05-08 08:47:50 -0700

    use Result type in not-quite-file representation

--- in_action/ch_3_compound_data/not-quite-file/src/main.rs
+++ in_action/ch_3_compound_data/not-quite-file/src/main.rs
@@ -1,4 +1,9 @@
-#![allow(unused_variables)]             //Silences the warnings caused by open and close
+extern crate rand;                      //bring rand crate into scope
+use rand::Rng;                          //bring random number generator trait into scope
+
+fn one_in(n: u32) -> bool {
+    return rand::thread_rng().gen_weighted_bool(n);    //helper function to trigger sporadic errors
+}
 
 #[derive(Debug)]                        // Enables file to work with printlnl! and its fmt! siblings       
 struct File {
@@ -20,38 +25,46 @@ impl File {
         return f;
     }
 
-    fn read(self: &File, save_to: &mut Vec<u8>) -> usize { //return number of bytes read
+    fn read(self: &File, save_to: &mut Vec<u8>) -> Result<usize, String> { //return number of bytes read
         let mut tmp = self.data.clone();                   //make a copy of the data here
         let read_length = tmp.len();
         save_to.reserve(read_length);                   //not necessary but usefulto know about. Ensures there is sufficient space to fit theincoming data
         save_to.append(&mut tmp);                       // allocate sufficent data in the save_to buffer to hold the contents of f
-        return read_length;
+        Ok(read_length)
     }
+
     
 }
-
-fn open(f: &mut File) -> bool {
-    true
+fn open(f: File) -> Result<File, String> {
+    if one_in(10_000) {
+        let err_msg = String::from("Permission denied");
+        return Err(err_msg);
+    }
+    Ok(f)
 }
 
-fn close(f: &mut File) -> bool {
-    true
+fn close(f: File) -> Result<File, String> {
+    if one_in(100_000) {
+        let err_msg = String::from("Interrupted by signal");
+        return Err(err_msg);
+    }
+    Ok(f)
 }
 
 
 fn main() {
-    let f3_data: Vec<u8> = vec![114, 117, 115, 116, 33];
-    let mut f3 = File::new_with_data("2.txt", &f3_data);
+    let f4_data: Vec<u8> = vec![114, 117, 115, 116, 33];
+    let mut f4 = File::new_with_data("4.txt", &f4_data);
 
     let mut buffer: Vec<u8> = vec![];
 
-    open(&mut f3);
-    let f3_length = &f3.read(&mut buffer);
-    close(&mut f3);
+    f4 = open(f4).unwrap();
+    let f4_length = f4.read(&mut buffer).unwrap();
+    f4 = close(f4).unwrap();
 
     let text = String::from_utf8_lossy(&buffer);    //converts Vec<u8> to String
 
-    println!("{:?}", f3);
-    println!("{} is {} bytes long", &f3.name, f3_length);
+    println!("{:?}", f4);
+    println!("{} is {} bytes long", &f4.name, f4_length);
     println!("{}", text);
 }
