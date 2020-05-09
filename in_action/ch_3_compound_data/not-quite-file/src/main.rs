dab1a63e8b43fc4461eeb585b962302a74066ba1
Leland Krych <leland.krych@gmail.com>
2020-05-09 08:14:34 -0700

    use an enum in our file type

--- in_action/ch_3_compound_data/not-quite-file/src/main.rs
+++ in_action/ch_3_compound_data/not-quite-file/src/main.rs
@@ -1,14 +1,14 @@
-extern crate rand;                      //bring rand crate into scope
-use rand::Rng;                          //bring random number generator trait into scope
-
-fn one_in(n: u32) -> bool {
-    return rand::thread_rng().gen_weighted_bool(n);    //helper function to trigger sporadic errors
+#[derive(Debug, PartialEq)]
+enum FileState {
+    Open,
+    Closed,
 }
 
 #[derive(Debug)]                        // Enables file to work with printlnl! and its fmt! siblings       
 struct File {
     name: String,
-    data: Vec<u8>,                      //Using Vec<u8> provides access to some useful conveniences such as writing to a file
+    data: Vec<u8>,
+    state: FileState,
 }
 
 impl File {
@@ -16,16 +16,14 @@ impl File {
         File {
             name: String::from(name),
             data: Vec::new(),
+            state: FileState::Closed
         }
     }
 
-    fn new_with_data(name: &str, data: &Vec<u8>) -> File {
-        let mut f = File::new(name);
-        f.data = data.clone();
-        return f;
-    }
-
     fn read(self: &File, save_to: &mut Vec<u8>) -> Result<usize, String> { //return number of bytes read
+        if self.state != FileState::Open {
+            return Err(String::from("File must be open for reading"));
+        }
         let mut tmp = self.data.clone();                   //make a copy of the data here
         let read_length = tmp.len();
         save_to.reserve(read_length);                   //not necessary but usefulto know about. Ensures there is sufficient space to fit theincoming data
@@ -35,36 +33,33 @@ impl File {
 
     
 }
-fn open(f: File) -> Result<File, String> {
-    if one_in(10_000) {
-        let err_msg = String::from("Permission denied");
-        return Err(err_msg);
-    }
+fn open(mut f: File) -> Result<File, String> {
+    f.state = FileState::Open;
     Ok(f)
 }
 
-fn close(f: File) -> Result<File, String> {
-    if one_in(100_000) {
-        let err_msg = String::from("Interrupted by signal");
-        return Err(err_msg);
-    }
+fn close(mut f: File) -> Result<File, String> {
+    f.state = FileState::Closed;
     Ok(f)
 }
 
 
 fn main() {
-    let f4_data: Vec<u8> = vec![114, 117, 115, 116, 33];
-    let mut f4 = File::new_with_data("4.txt", &f4_data);
+    let mut f5 = File::new("5.txt");
 
     let mut buffer: Vec<u8> = vec![];
 
-    f4 = open(f4).unwrap();
-    let f4_length = f4.read(&mut buffer).unwrap();
-    f4 = close(f4).unwrap();
+    if f5.read(&mut buffer).is_err() {
+        println!("Error checking is working"); 
+    }
+
+    f5 = open(f5).unwrap();
+    let f5_length = f5.read(&mut buffer).unwrap();
+    f5 = close(f5).unwrap();
 
     let text = String::from_utf8_lossy(&buffer);    //converts Vec<u8> to String
 
-    println!("{:?}", f4);
-    println!("{} is {} bytes long", &f4.name, f4_length);
+    println!("{:?}", f5);
+    println!("{} is {} bytes long", &f5.name, f5_length);
     println!("{}", text);
 }
